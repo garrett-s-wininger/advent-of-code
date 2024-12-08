9494578ee67f1812ff399bc4cfca14e045e7d650
Garrett Wininger <garrett.s.wininger@outlook.com>
2024-12-07 23:35:03 -0500

    Update Python to UV for unified tooling

--- Python/2022/11.py
+++ Python/2022/11.py
@@ -1,40 +1,42 @@
 #!/usr/bin/env python3
 
 import sys
-
-from math import floor
 from typing import Callable, List, TextIO
+
 
 class Monkey:
     def __init__(
         self,
         items: List[int],
         operation: Callable[[int], int],
-        test: Callable[[int], int]
+        test: Callable[[int], int],
     ) -> None:
         self.inspection_count = 0
         self.items = items
         self.operation = operation
         self.test = test
 
+
 def parse_items(line: str) -> List[int]:
     return [int(item) for item in line.split("Starting items: ")[-1].split(", ")]
+
 
 def parse_operation(line: str) -> Callable[[int], int]:
     operation_components = line.split("Operation: new = old ")[-1].split(" ")
 
     if operation_components[0] == "+":
-        if not "old" in operation_components[1]:
+        if "old" not in operation_components[1]:
             return lambda x: x + int(operation_components[1])
         else:
             return lambda x: x + x
     elif operation_components[0] == "*":
-        if not "old" in operation_components[1]:
+        if "old" not in operation_components[1]:
             return lambda x: x * int(operation_components[1])
         else:
             return lambda x: x * x
     else:
         return lambda x: x
+
 
 def parse_test(lines: List[str]) -> Callable[[int], int]:
     divisor = int(lines[0].split("divisible by ")[-1])
@@ -43,21 +45,23 @@ def parse_test(lines: List[str]) -> Callable[[int], int]:
 
     return lambda x: true_destination if x % divisor == 0 else false_destination
 
+
 def read_monkeys(input_file: TextIO) -> List[Monkey]:
     monkeys: List[Monkey] = []
     lines = input_file.readlines()
 
     for x in range(0, len(lines), 7):
-        definition = lines[x:x+7]
+        definition = lines[x : x + 7]
         monkeys.append(
             Monkey(
                 parse_items(definition[1]),
                 parse_operation(definition[2]),
-                parse_test(definition[3:6])
+                parse_test(definition[3:6]),
             )
         )
 
     return monkeys
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -77,9 +81,7 @@ if __name__ == "__main__":
             monkey.items = []
 
     top_inspections = sorted(
-        [monkey.inspection_count for monkey in monkeys],
-        key=lambda x: x,
-        reverse=True
+        [monkey.inspection_count for monkey in monkeys], key=lambda x: x, reverse=True
     )
 
     print(f"Total Monkey Business: {top_inspections[0] * top_inspections[1]}")
